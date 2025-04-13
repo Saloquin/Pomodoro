b57d0db53e21af40f3a07993735316630fefd5cd
roger233 <thomas.roger@etu.unicaen.fr>
2025-04-14 01:56:47 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Pomodoro/', // Ajout de la base URL pour GitHub Pages
   plugins: [
     vue(),
   ],
